562b414296cb4b5ff0dd5c431dfd852effbc667f
Simant Gurung <simant.xdezo@gmail.com>
2023-09-28 23:31:15 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log ("Hello World")
+
+const a = 10
+const b = 20
+const sum = a+b
+console.log(sum)
+
+// Variable example
+const name = "Simant"
+console.log(name)
+
+// Array example
+const names = ["Ram", "Sam"]
+console.log(names[0])
+
+// Object example
+const dataofperson = {
+    name : "Rahul",
+    age : 20,
+    profession : "student"
+}
+
+console.log("His profession is: " + dataofperson.profession)
