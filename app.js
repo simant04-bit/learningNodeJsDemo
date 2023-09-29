b535afbc831ca69f676ebc908213e1f6814a3c10
Simant Gurung <simant.xdezo@gmail.com>
2023-09-29 12:11:32 +0545

    express and ports

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const express = require("express")
+const app = express() //express() bhannale initialize gareko ho, or call gareko bhaninxa
+
+// Alternative in single line
+// const app = require("express")() // () bhannale initialize gareko ho
+
 console.log ("Hello World")
 
 const a = 10
@@ -21,3 +27,18 @@ const dataofperson = {
 }
 
 console.log("His profession is: " + dataofperson.profession)
+
+app.get("/", (req,res)=>{
+    res.send("Hello. This is a new page.")
+})
+
+app.get("/about", (req,res)=>{
+    res.json({
+        message1: "This is about page"
+    })
+})
+
+app.listen(3000, (req, res)=>{
+    console.log("Nodejs has started at port 3000")
+})
+
